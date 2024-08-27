95faa8a3e4ebcdff04c8003dee240803098edc58
Jason Witt <jawittdesigns@gmail.com>
2024-08-27 10:19:48 -0400

    Added images to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
-	basePath: '/out/portfolio',
+	basePath: '/portfolio',
+	images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
