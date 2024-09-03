cf35b00624852fd20eeaead4bf2f698f1273b228
Jason Witt <jawittdesigns@gmail.com>
2024-09-03 11:39:17 -0400

    Maybe fixed avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -10,7 +10,7 @@ const UserAvatar = () => {
 				src="https://media.licdn.com/dms/image/v2/D5635AQFT8ZGD6W6vLg/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1715009033982?e=1725984000&v=beta&t=Hk-MwgUXQrIId70a_uiLI7-_1ZxdTKXLKNddeHsSmcc"
 				alt="Jason Witt Profile Picture"
 			/>
-			<AvatarFallback>JW</AvatarFallback>
+			<AvatarFallback className="text-black">JW</AvatarFallback>
 		</Avatar>
 	 );
 }
